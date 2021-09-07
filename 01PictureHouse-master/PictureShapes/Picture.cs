fbb1fc2bd3119beece8e707b3cd5196fe71a02d3
Algot <algot@colmsjo.com>
2021-09-07 12:05:35 +0200

    hej

---
+++ 01PictureHouse-master/PictureShapes/Picture.cs
@@ -0,0 +1,47 @@
+﻿namespace PictureShapes
+{
+    class Picture
+    {
+        private Square wall;
+        private Triangle roof;
+        private Square window;
+        private Circle sol;
+        public Picture()
+        {
+            DrawImage();
+        }
+        public void DrawImage()
+        {
+            wall = new Square();
+            wall.MakeVisible();
+            wall.ChangeColor("red");
+            wall.ChangeSize(200);
+            wall.MoveHorizontal(70);
+            wall.MoveVertical(20);
+
+            roof = new Triangle();
+            roof.MakeVisible();
+            roof.ChangeColor("black");
+            roof.ChangeSize(260);
+            roof.MoveHorizontal(-13);
+            roof.MoveVertical(-10);
+
+            window = new Square();
+            window.MakeVisible();
+            window.ChangeColor("black");
+            window.ChangeSize(60);
+            window.MoveHorizontal(80);
+            window.MoveVertical(60);
+
+            sol = new Circle();
+            sol.MakeVisible();
+            sol.ChangeColor("yellow");
+            sol.ChangeSize(100);
+            sol.MoveHorizontal(475);
+            sol.MoveVertical(30);
+
+
+
+        }
+    }
+}
---
+++ dice/PictureShapes/Dice.cs
@@ -0,0 +1,85 @@
+﻿namespace PictureShapes
+{
+    class Dice
+    {
+        private Square dice;
+        private Circle prick;
+        private Circle prick2;
+        private Circle prick3;
+        private Circle prick4;
+        private Circle prick5;
+        private Circle prick6;
+        private Circle prick7;
+
+        public void Roll()
+        {
+            
+        }
+        
+        public Dice()
+        {
+            DrawImage();
+        }
+        public void DrawImage()
+        {
+            dice = new Square();
+            dice.MakeVisible();
+            dice.ChangeColor("white");
+            dice.ChangeSize(450);
+            dice.MoveHorizontal(70);
+            dice.MoveVertical(-150);
+
+            prick = new Circle();
+            prick.MakeVisible();
+            prick.ChangeColor("red");
+            prick.ChangeSize(85);
+            prick.MoveHorizontal(360);
+            prick.MoveVertical(40);
+
+            prick2 = new Circle();
+            prick2.MakeVisible();
+            prick2.ChangeColor("red");
+            prick2.ChangeSize(85);
+            prick2.MoveHorizontal(360);
+            prick2.MoveVertical(180);
+
+            prick3 = new Circle();
+            prick3.MakeVisible();
+            prick3.ChangeColor("red");
+            prick3.ChangeSize(85);
+            prick3.MoveHorizontal(360);
+            prick3.MoveVertical(320);
+
+            prick4 = new Circle();
+            prick4.MakeVisible();
+            prick4.ChangeColor("red");
+            prick4.ChangeSize(85);
+            prick4.MoveHorizontal(220);
+            prick4.MoveVertical(180);
+
+            prick5 = new Circle();
+            prick5.MakeVisible();
+            prick5.ChangeColor("red");
+            prick5.ChangeSize(85);
+            prick5.MoveHorizontal(80);
+            prick5.MoveVertical(180);
+
+            prick6 = new Circle();
+            prick6.MakeVisible();
+            prick6.ChangeColor("red");
+            prick6.ChangeSize(85);
+            prick6.MoveHorizontal(80);
+            prick6.MoveVertical(320);
+
+            prick7 = new Circle();
+            prick7.MakeVisible();
+            prick7.ChangeColor("red");
+            prick7.ChangeSize(85);
+            prick7.MoveHorizontal(80);
+            prick7.MoveVertical(40);
+
+
+
+        }
+    }
+}
