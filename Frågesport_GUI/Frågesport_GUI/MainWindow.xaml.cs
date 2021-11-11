443d71c07c1246f11b85abb36b9681bcbd66b254
Algot Colmsjö <algot@colmsjo.com>
2021-11-11 13:58:06 +0100

    hej

---
+++ Frågesport_GUI/Frågesport_GUI/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Frågesport_GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Kortlek frågor = new Kortlek();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+
+        private void startKnapp_Click(object sender, RoutedEventArgs e)
+        {
+            ModerKort mittKort = frågor.Drakort();
+            fråga.Text = mittKort.Fråga;
+            förstaSida.Children.Remove(startKnapp);
+            förstaSida.Children.Remove(välkommen);
+            förstaSida.Children.Remove(emptySpace);
+            svar1.Content = mittKort.GetSvar();
+            List<String> Flersvar = ((Flersvar)mittKort).Getflersvar();
+            svar2.Content = Flersvar[0];
+            svar3.Content = Flersvar[1];
+            svar4.Content = Flersvar[2];
+        }
+    }
+}
